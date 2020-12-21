cb58b9fac70349e7cb9b2ba83c4331817a294d13
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-12-21 14:55:18 -0300

    Add error 500 response

--- src/server/request/handlers.js
+++ src/server/request/handlers.js
@@ -3,9 +3,14 @@ const { getProductData } = require("../../domain/aggregate");
 const lastTimeRequestedCache = new Map();
 
 const handleProductRequest = async (req, res) => {
-  const url = decodeURIComponent(req.params.url);
-  const product = await getProductData(lastTimeRequestedCache)(url);
-  return res.json(product);
+  try {
+    const url = decodeURIComponent(req.params.url);
+    const product = await getProductData(lastTimeRequestedCache)(url);
+    return res.json(product);
+  } catch (e) {
+    console.error(e.message);
+    return res.status(500).json({ error: e.message });
+  }
 };
 
 module.exports = {
